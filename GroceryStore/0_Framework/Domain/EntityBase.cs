5a14d391f81baf5667c62cb889862f1ff1fe75e9
rezanabhani387 <rezanabhani387@gmail.com>
2021-12-28 21:31:23 +0330

    پیاده سازی لایه Domain و Application Contract های گروه محصول

---
+++ GroceryStore/0_Framework/Domain/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace _0_Framework.Domain
+{
+    public class EntityBase
+    {
+        public long ID { get; set; }
+        public DateTime CreationDate { get; set; }
+
+
+        public EntityBase()
+        {
+            CreationDate =  DateTime.Now;
+        }
+    }
+}
