d1f7a22bf57b78151c5276d1dedad7bbba73959f
rezanabhani387 <rezanabhani387@gmail.com>
2021-12-31 21:47:06 +0330

    ساخت Mapping و ProductRepository در لایه Infrastructure

--- GroceryStore/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
+++ GroceryStore/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
@@ -1,4 +1,6 @@
 ﻿using _0_Framework.Domain;
+using ShopManagement.Domain.ProductAgg;
+using System.Collections.Generic;
 
 namespace ShopManagement.Domain.ProductCategoryAgg
 {
@@ -10,11 +12,18 @@ namespace ShopManagement.Domain.ProductCategoryAgg
         public string PictureAlt { get; private set; }
         public string PictureTitle { get; private set; }
         public string Keywords { get; private set; }
-        public string MetaDescription { get;private  set; }
+        public string MetaDescription { get; private set; }
         public string Slug { get; private set; }
+        public List<Product> Products { get; private set; }
+
+
+        public ProductCategory()
+        {
+            Products = new List<Product>();
+        }
 
         public ProductCategory(string name, string description, string picture, string pictureAlt,
-            string pictureTitle,string keywords,string metaDescription,string slug)
+            string pictureTitle, string keywords, string metaDescription, string slug)
         {
             Name = name;
             Description = description;
