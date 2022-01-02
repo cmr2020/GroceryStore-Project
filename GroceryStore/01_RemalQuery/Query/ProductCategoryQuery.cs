7b7ada68dc2c1e02dfe5adf7efdccc4e33003fab
rezanabhani387 <rezanabhani387@gmail.com>
2022-01-03 02:22:59 +0330

    نمایش محصولات و اسلایدر در سایت

---
+++ GroceryStore/01_RemalQuery/Query/ProductCategoryQuery.cs
@@ -0,0 +1,31 @@
+﻿using _01_RemalQuery.Contracts.ProductCategory;
+using ShopManagement.Infrastructure.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_RemalQuery.Query
+{
+    public class ProductCategoryQuery : IProductCategoryQuery
+    {
+        private readonly ShopContext _context;
+
+        public ProductCategoryQuery(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public List<ProductCategoryQueryModel> GetProductCategories()
+        {
+            return _context.ProductCategories.Select(x => new ProductCategoryQueryModel
+            {
+                Id = x.ID,
+                Name = x.Name,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                Slug = x.Slug
+            }).ToList();
+        }
+    }
+}
