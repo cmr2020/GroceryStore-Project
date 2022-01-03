d727f5592c50623bff2e6ccd62ad4f18db26e827
rezanabhani387 <rezanabhani387@gmail.com>
2022-01-03 15:16:20 +0330

    پیاده سازی Mapping و Repository تخفیفات مشتری

---
+++ GroceryStore/DiscountManagement.Application/CustomerDiscountApplication.cs
@@ -0,0 +1,41 @@
+﻿using _0_Framework.Application;
+using DiscountManagement.Application.Contract.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscountManagement.Application
+{
+    class CustomerDiscountApplication : ICustomerDiscountApplication
+    {
+        private readonly ICustomerDiscountRepository _customerDiscountRepository;
+
+        public CustomerDiscountApplication(ICustomerDiscountRepository customerDiscountRepository)
+        {
+            _customerDiscountRepository = customerDiscountRepository;
+        }
+
+        public OperationResult Define(DefineCustomerDiscount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditCustomerDiscount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditCustomerDiscount GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GroceryStore/DiscountManagement.Infrastructure.EFCore/Repository/CustomerDiscountRepository.cs
@@ -0,0 +1,75 @@
+﻿using _0_Framework.Infrastructure;
+using DiscountManagement.Application.Contract.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using ShopManagement.Infrastructure.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+
+namespace DiscountManagement.Infrastructure.EFCore.Repository
+{
+    public class CustomerDiscountRepository : RepositoryBase<long, CustomerDiscount>, ICustomerDiscountRepository
+    {
+        private readonly DiscountContext _context;
+        private readonly ShopContext _shopContext;
+        public CustomerDiscountRepository(DiscountContext context, ShopContext shopContext) : base(context)
+        {
+            _context = context;
+            _shopContext = shopContext;
+        }
+
+        public EditCustomerDiscount GetDetails(long id)
+        {
+            return _context.CustomerDiscounts.Select(x => new EditCustomerDiscount
+            {
+                Id = x.ID,
+                ProductId = x.ProductId,
+                DiscountRate = x.DiscountRate,
+                StartDate = x.StartDate.ToString(),
+                EndDate = x.EndDate.ToString(),
+                Reason = x.Reason
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel)
+        {
+
+            var products = _shopContext.Products.Select(x => new { x.ID, x.Name }).ToList();
+            var query = _context.CustomerDiscounts.Select(x => new CustomerDiscountViewModel
+            {
+                Id = x.ID,
+                DiscountRate = x.DiscountRate,
+                EndDate = x.EndDate.ToString(),
+                EndDateGr = x.EndDate,
+                StartDate = x.StartDate.ToString(),
+                StartDateGr = x.StartDate,
+                ProductId = x.ProductId,
+                Reason = x.Reason,          
+            });
+
+            if (searchModel.ProductId > 0)
+                query = query.Where(x => x.ProductId == searchModel.ProductId);
+
+            if (!string.IsNullOrWhiteSpace(searchModel.StartDate))
+            {
+                var startDate = DateTime.Now;
+                query = query.Where(x => x.StartDateGr > startDate);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchModel.EndDate))
+            {
+                var EndDate = DateTime.Now;
+                query = query.Where(x => x.EndDateGr < EndDate);
+            }
+
+            var discounts = query.OrderByDescending(x => x.Id).ToList();
+
+            discounts.ForEach(discount =>
+                discount.Product = products.FirstOrDefault(x => x.ID == discount.ProductId)?.Name);
+
+            return discounts;
+        }
+    }
+}
