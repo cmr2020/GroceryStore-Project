5c3420389ef4c41520a2d30de38291cb47636c5b
rezanabhani387 <rezanabhani387@gmail.com>
2022-01-04 15:24:23 +0330

    پیاده سازی Repository انبارداری و Migration

--- GroceryStore/InventoryManagement.Domain/InventoryAgg/Inventory.cs
+++ GroceryStore/InventoryManagement.Domain/InventoryAgg/Inventory.cs
@@ -1,5 +1,4 @@
 ﻿using _0_Framework.Domain;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -51,33 +50,6 @@ namespace InventoryManagement.Domain.InventoryAgg
         }
 
 
-    }
-
-
-    public class InventoryOperation
-    {
-        public long Id { get; private set; }
-        public bool Operation { get; private set; }
-        public long Count { get; private set; }
-        public long OperatorId { get; private set; }
-        public DateTime OperationDate { get; private set; }
-        public long CurrentCount { get; private set; }
-        public string Description { get; private set; }
-        public long OrderId { get; private set; }
-        public long InventoryId { get; private set; }
-        public Inventory Inventory { get; private set; }
-
-        public InventoryOperation(bool operation, long count, long operatorId,
-            long currentCount, string description, long orderId, long inventoryId)
-        {
-            Operation = operation;
-            Count = count;
-            OperatorId = operatorId;
-            CurrentCount = currentCount;
-            Description = description;
-            OrderId = orderId;
-            InventoryId = inventoryId;
-        }
     }
 
 }
---
+++ GroceryStore/InventoryManagement.Infrastructure.Configuration/InventoryManagementBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using InventoryManagement.Application.Contract.Inventory;
+using InventoryManagement.Domain.InventoryAgg;
+using InventoryMangement.Infrastructure.EFCore;
+using InventoryMangement.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Infrastructure.Configuration
+{
+    public class InventoryManagementBootstrapper
+    {
+
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+
+            services.AddTransient<IInventoryRepository, InventoryRepository>();
+
+
+            services.AddDbContext<InventoryContext>(x => x.UseSqlServer(connectionString));
+        }
+    }
+}
