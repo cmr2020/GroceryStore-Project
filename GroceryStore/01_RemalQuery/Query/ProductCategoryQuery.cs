e2c5d9afb4628e3ff6667ad190e353143896e2ba
rezanabhani387 <rezanabhani387@gmail.com>
2022-01-05 18:10:38 +0330

    نمایش قیمت محصول در صفحه اصلی

--- GroceryStore/01_RemalQuery/Query/ProductCategoryQuery.cs
+++ GroceryStore/01_RemalQuery/Query/ProductCategoryQuery.cs
@@ -1,5 +1,7 @@
-﻿using _01_RemalQuery.Contracts.Product;
+﻿using _0_Framework.Application;
+using _01_RemalQuery.Contracts.Product;
 using _01_RemalQuery.Contracts.ProductCategory;
+using InventoryMangement.Infrastructure.EFCore;
 using Microsoft.EntityFrameworkCore;
 using ShopManagement.Domain.ProductAgg;
 using ShopManagement.Infrastructure.EFCore;
@@ -12,10 +14,12 @@ namespace _01_RemalQuery.Query
     public class ProductCategoryQuery : IProductCategoryQuery
     {
         private readonly ShopContext _context;
+        private readonly InventoryContext _inventoryContext;
 
-        public ProductCategoryQuery(ShopContext context)
+        public ProductCategoryQuery(ShopContext context, InventoryContext inventoryContext)
         {
             _context = context;
+            _inventoryContext = inventoryContext;
         }
 
         public List<ProductCategoryQueryModel> GetProductCategories()
@@ -33,38 +37,50 @@ namespace _01_RemalQuery.Query
 
         public List<ProductCategoryQueryModel> GetProductCategoriesWithProducts()
         {
-            return _context.ProductCategories
-                 .Include(x => x.Products)
-                 .ThenInclude(x => x.Category)
-                 .Select(x => new ProductCategoryQueryModel
-                 {
-                     Id = x.ID,
-                     Name = x.Name,
-                     Products = MapProducts(x.Products)
-                 }).AsNoTracking().ToList();
+            var inventory = _inventoryContext.Inventory.Select(x =>
+                new { x.ProductId, x.UnitPrice }).ToList();
+
+            var categories = _context.ProductCategories
+                .Include(x => x.Products)
+                .ThenInclude(x => x.Category)
+                .Select(x => new ProductCategoryQueryModel
+                {
+                    Id = x.ID,
+                    Name = x.Name,
+                    Products = MapProducts(x.Products)
+                }).AsNoTracking().ToList();
+
+            foreach (var category in categories)
+            {
+                foreach (var product in category.Products)
+                {
+                    var productInventory = inventory.FirstOrDefault(x => x.ProductId == product.Id);
+                    if (productInventory != null)
+                    {                  
+                        product.Price = inventory.FirstOrDefault(x => x.ProductId == product.Id)?
+                            .UnitPrice.ToMoney();                
+                       
+                    }
+                }
+            }
+
+
+            return categories;
         }
 
         private static List<ProductQueryModel> MapProducts(List<Product> products)
         {
 
-            var result = new List<ProductQueryModel>();
-
-            foreach (var product in products)
+            return products.Select(product => new ProductQueryModel
             {
-                var item = new ProductQueryModel
-                {
-                    Id = product.ID,
-                    Category = product.Category.Name,
-                    Name = product.Name,
-                    Picture = product.Picture,
-                    PictureAlt = product.PictureAlt,
-                    PictureTitle = product.PictureTitle,
-                    Slug = product.Slug
-
-                };
-                result.Add(item);
-            }
-            return result;
+                Id = product.ID,
+                Category = product.Category.Name,
+                Name = product.Name,
+                Picture = product.Picture,
+                PictureAlt = product.PictureAlt,
+                PictureTitle = product.PictureTitle,
+                Slug = product.Slug
+            }).ToList();
         }
 
     }
