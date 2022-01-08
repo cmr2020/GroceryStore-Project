8e33c664ff0434021969782012a81fb6986304e9
rezanabhani387 <rezanabhani387@gmail.com>
2022-01-08 15:20:10 +0330

    Mapping و Repository دامین ArticleCategory

--- GroceryStore/BlogManagement.Application.Contracts/ArticleCategory/ArticleCategoryViewModel.cs
+++ GroceryStore/BlogManagement.Application.Contracts/ArticleCategory/ArticleCategoryViewModel.cs
@@ -1,7 +1,8 @@
 ﻿namespace BlogManagement.Application.Contracts.ArticleCategory
 {
     public class ArticleCategoryViewModel
-    {        
+    {
+        public long Id { get; set; }
         public string Name { get; set; }
         public string Picture { get; set; }
         public string Description { get; set; }
---
+++ GroceryStore/BlogManagement.Infrastructure.EFCore/Repository/ArticleCategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using _0_Framework.Infrastructure;
+using BlogManagement.Application.Contracts.ArticleCategory;
+using BlogManagement.Domain.ArticleCategoryAgg;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlogManagement.Infrastructure.EFCore.Repository
+{
+    public class ArticleCategoryRepository : RepositoryBase<long, ArticleCategory>, IArticleCategoryRepository
+    {
+        private readonly BlogContext _context;
+
+        public ArticleCategoryRepository(BlogContext context) : base(context)
+        {
+            _context = context;
+        }
+
+
+        public EditArticleCategory GetDetails(long id)
+        {
+            return _context.ArticleCategories.Select(x => new EditArticleCategory
+            {
+                Id = x.ID,
+                Name = x.Name,
+                CanonicalAddress = x.CanonicalAddress,
+                Description = x.Description,
+                Keywords = x.Keywords,
+                MetaDescription = x.MetaDescription,
+                ShowOrder = x.ShowOrder,
+                Slug = x.Slug,              
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel)
+        {
+            var query = _context.ArticleCategories                
+                 .Select(x => new ArticleCategoryViewModel
+                 {
+                     Id = x.ID,
+                     Description = x.Description,
+                     Name = x.Name,
+                     Picture = x.Picture,
+                     ShowOrder = x.ShowOrder,                 
+                 });
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Name))
+                query = query.Where(x => x.Name.Contains(searchModel.Name));
+
+            return query.OrderByDescending(x => x.ShowOrder).ToList();
+        }
+    }
+}
