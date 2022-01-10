0a1b9b5a8fb4b3434db01bdb5f47906a004bc5fb
rezanabhani387 <rezanabhani387@gmail.com>
2022-01-10 18:54:14 +0330

    ایجاد زیرسیستم Account Management

---
+++ GroceryStore/AccountManagement.Application.Contracts/Account/AccountSearchModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AccountManagement.Application.Contracts.Account
+{
+    public class AccountSearchModel
+    {
+        public string Fullname { get; set; }
+        public string Username { get; set; }
+        public string Mobile { get; set; }
+        public long RoleId { get; set; }
+    }
+
+
+}
---
+++ GroceryStore/AccountManagement.Application.Contracts/Account/EditAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace AccountManagement.Application.Contracts.Account
+{
+    public class EditAccount : RegisterAccount
+    {
+        public long Id { get; set; }
+    }
+
+
+}
