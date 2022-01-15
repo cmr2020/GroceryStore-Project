c8339266a45472f08b398d148ddfed5ae5fa58c0
rezanabhani387 <rezanabhani387@gmail.com>
2022-01-15 17:48:31 +0330

    دریافت Permission های هر Role

---
+++ GroceryStore/ServiceHost/Areas/Administration/Pages/Accounts/Role/Edit.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccountManagement.Application.Contracts.Role;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.Administration.Pages.Accounts.Role
+{
+    public class EditModel : PageModel
+    {
+
+        public EditRole Command;
+        private readonly IRoleApplication _roleApplication;
+      
+        public EditModel(IRoleApplication roleApplication)
+        {
+            _roleApplication = roleApplication;          
+        }
+        public void OnGet(long id)
+        {
+            Command = _roleApplication.GetDetails(id);
+        }
+
+        public IActionResult OnPost(EditRole command)
+        {
+            var result = _roleApplication.Edit(command);
+            return RedirectToPage("Index");
+        }
+    }
+}
