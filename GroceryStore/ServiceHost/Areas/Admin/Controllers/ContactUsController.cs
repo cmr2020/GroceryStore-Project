6394427da50ab2b499f1d5796bfd20f0d6d1d3a1
rezanabhani387 <rezanabhani387@gmail.com>
2022-02-24 20:27:58 +0330

    Create Admin ContactUs

---
+++ GroceryStore/ServiceHost/Areas/Admin/Controllers/ContactUsController.cs
@@ -0,0 +1,32 @@
+﻿using _0_Framework.Infrastructure;
+using AccountMangement.Infrastructure.EFCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceHost.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = Roles.Administrator)]
+    public class ContactUsController : Controller
+    {
+
+        private readonly AccountContext _context;
+
+        public ContactUsController(AccountContext context)
+        {
+            _context = context;
+        }
+
+
+       
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ContactUses.ToListAsync());
+        }
+    }
+}
--- GroceryStore/ServiceHost/Startup.cs
+++ GroceryStore/ServiceHost/Startup.cs
@@ -119,6 +119,9 @@ namespace ServiceHost
             {
                 endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                    name: "default",
                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
